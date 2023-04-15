cf0d8a24820fc36435c430fe8ed75fe31c1c41b9
Torstein Lunde <lunde888@gmail.com>
2023-04-15 17:53:21 +0200

    Return to active folder after secrets are read.

--- Secrets.py
+++ Secrets.py
@@ -14,6 +14,9 @@ class SecretsCls:
 
     #########################################
     def load(self):
+        # Remember the current working directory
+        cwd = os.getcwd()
+
         # Set the path to current folder
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,6 +29,9 @@ class SecretsCls:
                     continue    
                 key, value = line.split('=')
                 self.secrets[key] = value
+        
+        # Set the path back to the original working directory
+        os.chdir(cwd)
     
     #########################################
     def getSecret(self, key):
